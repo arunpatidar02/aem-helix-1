9fd2fb2f83ee0f60136119f0081196776ca4e6d7
Arun Patidar <arun.patidar@accenture.com>
2023-11-21 17:28:04 +0100

    list

--- tools/sidekick/plugins/tags2/tags2.js
+++ tools/sidekick/plugins/tags2/tags2.js
@@ -1,102 +1,10 @@
-/* eslint-disable no-use-before-define */
 /* eslint-disable no-plusplus */
-/* eslint-disable guard-for-in */
-/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-use-before-define */
 // eslint-disable-next-line import/no-unresolved
 import { PLUGIN_EVENTS } from 'https://www.hlx.live/tools/sidekick/library/events/events.js';
+import UTILS from './utils.js';
 
-const selectedTags = [];
-
-function getSelectedLabel() {
-  return selectedTags.length > 0 ? `${selectedTags.length} tag${selectedTags.length > 1 ? 's' : ''} selected` : 'No tags selected';
-}
-
-function getFilteredTags(data, query) {
-  if (!query) {
-    return data;
-  }
-  const filteredObj = [];
-  function filterRecursive(obj) {
-    for (const key in obj) {
-      if (key.toLowerCase().includes(query.toLowerCase())) {
-        filteredObj.push(key);
-      }
-      if (typeof obj[key] === 'object') {
-        filterRecursive(obj[key]);
-      }
-    }
-  }
-
-  filterRecursive(data);
-
-  return filteredObj;
-}
-
-function removeUndefined(obj) {
-  for (const key in obj) {
-    if (key === 'undefined') {
-      delete obj[key];
-    } else if (typeof obj[key] === 'object') {
-      // Recursively check nested objects
-      removeUndefined(obj[key]);
-    }
-  }
-}
-
-function removeColumnContent(colEle) {
-  const list = colEle.querySelector('ul');
-  if (list) {
-    colEle.removeChild(list);
-  }
-}
-
-function findObjectByKey(json, keyToFind) {
-  let result = null;
-
-  function searchObject(obj, key) {
-    for (const currentKey in obj) {
-      if (currentKey === key) {
-        result = obj[currentKey];
-        break;
-      } else if (typeof obj[currentKey] === 'object') {
-        searchObject(obj[currentKey], key);
-      }
-    }
-  }
-
-  searchObject(json, keyToFind);
-  return result;
-}
-
-function convertFlatArrayToHierarchy(flatArray) {
-  const hierarchy = {};
-  let stack = [];
-
-  flatArray.forEach((item) => {
-    let currentLevel = hierarchy;
-
-    for (let i = 0; i <= 5; i++) {
-      const key = i === 0 ? 'tag-category' : `level${i}`;
-      const value = item[key];
-
-      if (value !== '') {
-        // If the value is not empty, add it to the current level or at category level
-        if (i === 0) {
-          stack = [];
-        }
-        currentLevel[value] = currentLevel[value] || {};
-        currentLevel = currentLevel[value];
-        stack[i - 1] = value;
-      } else {
-        // If the value is empty, use the previous non-empty value
-        currentLevel[stack[i - 1]] = currentLevel[stack[i - 1]] || {};
-        currentLevel = currentLevel[stack[i - 1]];
-      }
-    }
-  });
-
-  return hierarchy;
-}
+let selectedTags = [];
 
 export async function decorate(container, data, query) {
   if (!data) {
@@ -105,29 +13,40 @@ export async function decorate(container, data, query) {
     return;
   }
 
-  // const dataObj = data;
-  const dataObj = convertFlatArrayToHierarchy(data);
-  removeUndefined(dataObj);
+  const dataObj = UTILS.getJsonObject(data);
 
-  const createMenuItems = () => {
-    const filteredTags = getFilteredTags(dataObj, query);
-    let resultString = '';
-    filteredTags.forEach((tag) => {
-      const isSelected = selectedTags.includes(tag);
-      resultString += `
-        <sp-menu-item class="tag-item-wrapper">
-          <ion-icon name="pricetag-outline"></ion-icon>
-          <ion-icon name="pricetag"></ion-icon>
-          <span value="${tag}" ${isSelected ? 'selected' : ''}>${tag}</span>
-        </sp-menu-item>`;
+  const createSearchItems = () => {
+    selectedTags = [];
+    const srConatiner = container.querySelector('.search-result-column');
+    const filteredTags = UTILS.getFilteredTags(dataObj, query);
+
+    const spMenu = document.createElement('ul');
+    srConatiner?.appendChild(spMenu);
+
+    filteredTags.forEach((key) => {
+      const spMenuItem = document.createElement('li');
+      spMenuItem.className = 'column-item';
+      const tagItem = document.createElement('p');
+      tagItem.className = 'tag-item';
+      tagItem.ariaChecked = 'false';
+      tagItem.value = key;
+      tagItem.isSelected = false;
+      const tagItemElements = `
+        <ion-icon name="pricetag-outline"></ion-icon>
+        <ion-icon name="pricetag"></ion-icon>
+        <span value="${key}">${key}</span>
+      `;
+      tagItem.innerHTML = tagItemElements;
+      spMenuItem.appendChild(tagItem);
+      tagItem.addEventListener('click', handleColumnItemClick);
+
+      spMenu.appendChild(spMenuItem);
     });
-
-    return resultString;
   };
 
   const createColumnMenu = () => {
     if (query) {
-      return;
+      createSearchItems();
     }
     const parentElement = container.querySelector('.category');
     createNavigation(null, parentElement);
@@ -150,7 +69,31 @@ export async function decorate(container, data, query) {
     }
 
     const selectedLabel = container.querySelector('.selectedLabel');
-    selectedLabel.textContent = getSelectedLabel();
+    selectedLabel.textContent = UTILS.getSelectedLabel(selectedTags);
+  };
+
+  const handleColumnItemClick = (e) => {
+    let ele = e.target;
+    const tagName = ele.tagName.toLowerCase();
+    if (tagName !== 'p') {
+      ele = ele.parentElement;
+    }
+
+    const { value } = ele;
+    const selected = ele.ariaChecked === 'true';
+    if (selected) {
+      const index = selectedTags.indexOf(value);
+      if (index > -1) {
+        selectedTags.splice(index, 1);
+      }
+      ele.ariaChecked = 'false';
+    } else {
+      selectedTags.push(value);
+      ele.ariaChecked = 'true';
+    }
+
+    const selectedLabel = container.querySelector('.selectedLabel');
+    selectedLabel.textContent = UTILS.getSelectedLabel(selectedTags);
   };
 
   const handleCopyButtonClick = () => {
@@ -165,17 +108,14 @@ export async function decorate(container, data, query) {
   let sp = /* html */`
   <div class="footer">
       <sp-icon-info slot="icon"></sp-icon-info>
-      <span class="selectedLabel">${getSelectedLabel()}</span>
+      <span class="selectedLabel">${UTILS.getSelectedLabel(selectedTags)}</span>
       <sp-action-button label="Copy" quiet>
         <sp-icon-copy slot="icon"></sp-icon-copy>
       </sp-action-button>
     </div>
     <sp-divider size="s"></sp-divider>`;
   if (query) {
-    const menuItems = createMenuItems();
-    sp += `<div class="search-result-column">
-    <sp-menu label="Select tags" selects="multiple">${menuItems}</sp-menu>
-    </div>`;
+    sp += '<div class="search-result-column"></div>';
   } else {
     sp += `
     <div class="menu-columns">
@@ -196,52 +136,65 @@ export async function decorate(container, data, query) {
   createColumnMenu();
 
   function createNavigation(parentKey, parentElement) {
-    const spMenu = document.createElement('sp-menu');
+    selectedTags = [];
+    const spMenu = document.createElement('ul');
     parentElement?.appendChild(spMenu);
-    const elementJson = parentKey ? findObjectByKey(dataObj, parentKey) : dataObj;
+    const elementJson = parentKey ? UTILS.findObjectByKey(dataObj, parentKey) : dataObj;
 
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
     for (const key in elementJson) {
       const obj = elementJson[key];
       const objKeys = Object.keys(obj);
-      const spMenuItem = document.createElement('sp-menu-item');
-      const tagItem = `
-      <ion-icon name="pricetag-outline"></ion-icon>
-      <ion-icon name="pricetag"></ion-icon>
-      <span value="${key}">${key}</span>`;
-
-      spMenuItem.innerHTML = tagItem;
-      if (!parentKey) {
-        spMenuItem.addEventListener('click', handleMenuItemClick);
-      }
+      const spMenuItem = document.createElement('li');
+      spMenuItem.className = 'column-item';
+      const tagItem = document.createElement('p');
+      tagItem.className = 'tag-item';
+      tagItem.ariaChecked = 'false';
+      tagItem.value = key;
+      tagItem.isSelected = false;
+      const tagItemElements = `
+        <ion-icon name="pricetag-outline"></ion-icon>
+        <ion-icon name="pricetag"></ion-icon>
+        <span value="${key}">${key}</span>
+      `;
+      tagItem.innerHTML = tagItemElements;
+      spMenuItem.appendChild(tagItem);
+      tagItem.addEventListener('click', handleColumnItemClick);
 
       if (objKeys.length > 0) {
         const icon = document.createElement('span');
         icon.className = 'chevron-right';
         spMenuItem.appendChild(icon);
-        addEventListeners(icon);
+        handleMenuExpand(icon);
       }
 
       spMenu.appendChild(spMenuItem);
     }
+
+    const selectedLabel = container.querySelector('.selectedLabel');
+    selectedLabel.textContent = UTILS.getSelectedLabel(selectedTags);
   }
 
-  function addEventListeners(element) {
+  function handleMenuExpand(element) {
     element.addEventListener('click', () => {
+      selectedTags = [];
+      const selectedLabel = container.querySelector('.selectedLabel');
+      selectedLabel.textContent = UTILS.getSelectedLabel(selectedTags);
       const { parentElement } = element;
       const isActive = parentElement.classList.contains('active');
-      const parentKey = parentElement.querySelector('sp-menu-item').value.trim();
+      const parentKey = parentElement.querySelector('.tag-item').value.trim();
       const curColEle = parentElement.closest('.column');
       const curColNum = Number(curColEle.dataset.col);
       const nextColEle = container.querySelector(`.subcategory[data-col="${curColNum + 1}"]`);
 
       if (isActive) {
-        removeColumnContent(nextColEle);
+        UTILS.removeColumnContent(nextColEle);
         curColEle.classList.remove('expanded');
       } else {
         for (let i = 5; i > curColNum; i--) {
           const colEle = container.querySelector(`.subcategory[data-col="${i}"]`);
           colEle.classList.remove('expanded');
-          removeColumnContent(colEle);
+          UTILS.removeColumnContent(colEle);
         }
         createNavigation(parentKey, nextColEle);
         curColEle.classList.add('expanded');
@@ -249,7 +202,11 @@ export async function decorate(container, data, query) {
         const siblingColEle = container.querySelector(`.column[data-col="${curColNum}"] li.active`);
         siblingColEle?.classList.remove('active');
       }
-    //  parentElement.classList.toggle('active');
+      const checkedItems = container.querySelectorAll('.tag-item[aria-checked="true"]');
+      checkedItems.forEach((item) => {
+        item.ariaChecked = 'false';
+      });
+      parentElement.classList.toggle('active');
     });
   }
 
---
+++ tools/sidekick/plugins/tags2/utils.js
@@ -0,0 +1,107 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+function getFilteredTags(data, query) {
+  if (!query) {
+    return data;
+  }
+  const filteredObj = [];
+  function filterRecursive(obj) {
+    for (const key in obj) {
+      if (key.toLowerCase().includes(query.toLowerCase())) {
+        filteredObj.push(key);
+      }
+      if (typeof obj[key] === 'object') {
+        filterRecursive(obj[key]);
+      }
+    }
+  }
+
+  filterRecursive(data);
+
+  return filteredObj;
+}
+
+function removeUndefined(obj) {
+  for (const key in obj) {
+    if (key === 'undefined') {
+      delete obj[key];
+    } else if (typeof obj[key] === 'object') {
+      // Recursively check nested objects
+      removeUndefined(obj[key]);
+    }
+  }
+}
+
+function findObjectByKey(json, keyToFind) {
+  let result = null;
+
+  function searchObject(obj, key) {
+    for (const currentKey in obj) {
+      if (currentKey === key) {
+        result = obj[currentKey];
+        break;
+      } else if (typeof obj[currentKey] === 'object') {
+        searchObject(obj[currentKey], key);
+      }
+    }
+  }
+
+  searchObject(json, keyToFind);
+  return result;
+}
+
+function convertFlatArrayToHierarchy(flatArray) {
+  const hierarchy = {};
+  let stack = [];
+
+  flatArray.forEach((item) => {
+    let currentLevel = hierarchy;
+
+    for (let i = 0; i <= 5; i++) {
+      const key = i === 0 ? 'tag-category' : `level${i}`;
+      const value = item[key];
+
+      if (value !== '') {
+        // If the value is not empty, add it to the current level or at category level
+        if (i === 0) {
+          stack = [];
+        }
+        currentLevel[value] = currentLevel[value] || {};
+        currentLevel = currentLevel[value];
+        stack[i - 1] = value;
+      } else {
+        // If the value is empty, use the previous non-empty value
+        currentLevel[stack[i - 1]] = currentLevel[stack[i - 1]] || {};
+        currentLevel = currentLevel[stack[i - 1]];
+      }
+    }
+  });
+
+  return hierarchy;
+}
+
+function getJsonObject(flatArray) {
+  const hierarchy = convertFlatArrayToHierarchy(flatArray);
+  removeUndefined(hierarchy);
+  return hierarchy;
+}
+
+function getSelectedLabel(selectedTags) {
+  return selectedTags.length > 0 ? `${selectedTags.length} tag${selectedTags.length > 1 ? 's' : ''} selected` : 'No tags selected';
+}
+
+function removeColumnContent(colEle) {
+  const list = colEle.querySelector('ul');
+  if (list) {
+    colEle.removeChild(list);
+  }
+}
+
+export default {
+  getJsonObject,
+  getFilteredTags,
+  findObjectByKey,
+  getSelectedLabel,
+  removeColumnContent,
+};
